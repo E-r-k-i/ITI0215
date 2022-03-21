c0c9cd475e87327687bed05ab651582f5e02c0fd
Erki Toom <erki.toom@taltech.ee>
2022-03-22 01:12:25 +0200

    connect to other hosts

--- src/main/java/services/AddressService.java
+++ src/main/java/services/AddressService.java
@@ -1,12 +1,14 @@
 package services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
-
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Scanner;
 
 public class AddressService implements HttpHandler {
 
@@ -23,7 +25,7 @@ public class AddressService implements HttpHandler {
 
             System.out.println(getIp(exchange));
 
-            writeIp(getIp(exchange));
+            getNeighbours(getIp(exchange));
 
             if (method.equals("GET")){
                 getRequest(exchange);
@@ -38,9 +40,54 @@ public class AddressService implements HttpHandler {
         }
     }
 
-    private void writeIp(String ip) {
-        //todo
+    private void getNeighbours(String ip) throws IOException {
+        // create a map
+        try {
+            // create object mapper instance
+            ObjectMapper mapper = new ObjectMapper();
+
+            // convert JSON file to map
+            Map<?, ?> map = mapper.readValue(Paths.get("C:/Users/Erki/IdeaProjects/ITI0215/Ledger/src/main/java/resources/conf.json").toFile(), Map.class);
+            // print map entries
+            String[] hosts = new String[0];
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + ":" + entry.getValue());
+                hosts = (entry.getValue().toString()).substring(1, entry.getValue().toString().length()-1).split(",");
+            }
+            for(String host : hosts){
+                host = host.trim();
+                System.out.println(host);
+                attemptConnection(host);
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
+
+    private void attemptConnection(String host) throws IOException {
+
+        String command = "curl -X GET "+host+"/addr";
+        //System.out.println(command);
+        //Process process = Runtime.getRuntime().exec(command);
+        ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+        Process process = pb.start();
+
+        InputStream is = process.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        StringBuilder responseStrBuilder = new StringBuilder();
+
+        String line = new String();
+
+        while ((line = br.readLine()) != null) {
+            System.out.println("read line from curl command: " + line);
+            responseStrBuilder.append(line);
+        }
+        is.close();
+
+    }
+
 
     private String getIp(HttpExchange exchange){
 
@@ -54,7 +101,7 @@ public class AddressService implements HttpHandler {
 
     private static void getRequest(HttpExchange exchange) throws IOException {
         URI requestURI = exchange.getRequestURI();
-        String response = requestURI.toString();
+        String response = requestURI.toString()+"asd";
 
         //System.out.println(response);
         exchange.sendResponseHeaders(200, response.getBytes().length);
